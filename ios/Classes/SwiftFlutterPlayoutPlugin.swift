df73aac7211c77a0852562920f0ea0e21e07347e
Nicolas Alves <niicolasalves@gmail.com>
2020-05-29 00:26:10 -0300

    Remoção do serviço de AUDIO (iOS)

--- ios/Classes/SwiftFlutterPlayoutPlugin.swift
+++ ios/Classes/SwiftFlutterPlayoutPlugin.swift
@@ -4,10 +4,7 @@ import UIKit
 public class SwiftFlutterPlayoutPlugin: NSObject, FlutterPlugin {
     
   public static func register(with registrar: FlutterPluginRegistrar) {
-    
-    /* register audio player */
-    AudioPlayer.register(with: registrar)
-    
+       
     /* register video player */
     VideoPlayerFactory.register(with: registrar)
   }
